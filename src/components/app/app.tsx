11890f4022de57463e43073e2aa6694a9225723f
Inna Makhova <i.m.makhova@gmail.com>
2021-12-23 19:53:52 +0200

    Создан browserHistory

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,18 @@
-import Catalog from '../catalog/catalog';
+import {Route, Router as BrowserRouter, Switch} from 'react-router-dom';
+import browserHistory from '../../browser-history';
+import CatalogPage from '../catalog-page/catalog-page';
+import {AppRoute} from '../../const';
 
 function App(): JSX.Element {
-  return <Catalog />;
+  return (
+    <BrowserRouter history={browserHistory}>
+      <Switch>
+        <Route exact path={AppRoute.Root}>
+          <CatalogPage />;
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
