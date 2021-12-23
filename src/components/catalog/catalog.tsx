85bfc4c3096ad6fb43d6e990fb62c343da90dd8c
Inna Makhova <i.m.makhova@gmail.com>
2021-12-23 15:37:49 +0200

    Добавлены компоненты Header, Footer и Catalog

---
+++ src/components/catalog/catalog.tsx
@@ -0,0 +1,13 @@
+import Header from '../common/header/header';
+import Footer from '../common/footer/footer';
+
+function Catalog(): JSX.Element {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Catalog;
