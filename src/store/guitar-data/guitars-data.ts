69d5ce4004cfece4213147a99091bab87e72a832
Inna Makhova <i.m.makhova@gmail.com>
2021-12-27 15:48:32 +0300

    Картинки перемещены в корень img

--- src/store/guitar-data/guitars-data.ts
+++ src/store/guitar-data/guitars-data.ts
@@ -1,17 +1,23 @@
 import {GuitarData} from '../../types/state';
 import {createReducer} from '@reduxjs/toolkit';
 import {
-  loadGuitars
+  loadGuitars, updateTotal
 } from '../action';
 
 const initialState: GuitarData = {
   guitars: [],
+  total: 0,
+  start: 0,
+  page: 1,
 };
 
 const guitarData = createReducer(initialState, (builder) => {
   builder
     .addCase(loadGuitars, (state, action) => {
       state.guitars = action.payload;
+    })
+    .addCase(updateTotal, (state, action) => {
+      state.total = action.payload;
     });
 });
 
