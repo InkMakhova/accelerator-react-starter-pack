61c68f0f314268354485000fa5f26c0691de6170
Inna Makhova <i.m.makhova@gmail.com>
2022-01-12 11:04:11 +0200

    Рефакторинг каталога, сброс фильтров

--- src/components/catalog-page/components/catalog/components/product-list/product-list.tsx
+++ src/components/catalog-page/components/catalog/components/product-list/product-list.tsx
@@ -1,7 +1,8 @@
-import {Guitar} from '../../../../../../types/guitar';
+import { Guitar } from '../../../../../../types/guitar';
 import ProductCard from './components/product-card/product-card';
-import {useQuery} from '../../../../../../hooks/use-query';
-import {useHistory, useLocation} from 'react-router-dom';
+import { useQuery } from '../../../../../../hooks/use-query';
+import { useHistory, useLocation } from 'react-router-dom';
+import { getNewPathname } from '../../../../../../util';
 
 type ItemListProps = {
   guitars: Guitar[],
@@ -32,10 +33,11 @@ function ProductList({guitars, guitarCount}: ItemListProps): JSX.Element {
         style={{cursor: 'pointer', color: '#c90606', border: 'none', background: 'transparent'}}
         onClick={(evt) => {
           evt.preventDefault();
-          query.delete('type[]');
           query.delete('price_gte');
           query.delete('price_lte');
+          query.delete('type[]');
           query.delete('stringCount[]');
+          location.pathname = getNewPathname(location.pathname);
           location.search = query.toString();
           history.push(`${location.pathname}?${location.search}`);
         }}
