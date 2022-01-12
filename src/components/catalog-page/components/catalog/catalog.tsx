e20be3c39583ee8c99fc074d9db4ecfd98e62700
Inna Makhova <i.m.makhova@gmail.com>
2022-01-12 23:15:48 +0200

    Рефакторинг пагинации, добавлен пропс количество страниц всего, вывод блока от 2 страниц

--- src/components/catalog-page/components/catalog/catalog.tsx
+++ src/components/catalog-page/components/catalog/catalog.tsx
@@ -1,21 +1,27 @@
 import Filter from './components/filter/filter';
 import Sort from './components/sort/sort';
-import {Guitar} from '../../../../types/guitar';
+import { Guitar } from '../../../../types/guitar';
 import ProductList from './components/product-list/product-list';
-import {ITEMS_PER_PAGE} from '../../../../const';
+import { ITEMS_PER_PAGE } from '../../../../const';
 import Pagination from './components/pagination/pagination';
+import { useSelector } from 'react-redux';
+import { getTotal } from '../../../../store/guitar-data/selectors';
 
 type CatalogProps = {
   guitars: Guitar[],
 }
 
 function Catalog({guitars}: CatalogProps): JSX.Element {
+  const totalGuitarNumber = useSelector(getTotal);
+
+  const totalPageNumber = Math.ceil(totalGuitarNumber / ITEMS_PER_PAGE);
+
   return (
     <div className="catalog">
       <Filter />
       <Sort />
       <ProductList guitars={guitars} guitarCount={ITEMS_PER_PAGE} />
-      <Pagination />
+      {totalPageNumber <= 1 ? '' : <Pagination totalPageNumber={totalPageNumber}/>}
     </div>
   );
 }
--- src/components/catalog-page/components/catalog/components/pagination/pagination.tsx
+++ src/components/catalog-page/components/catalog/components/pagination/pagination.tsx
@@ -1,28 +1,16 @@
-import { useSelector } from 'react-redux';
-import { getTotal } from '../../../../../../store/guitar-data/selectors';
-import { AppRoute, ITEMS_PER_PAGE, PAGINATION_STEP } from '../../../../../../const';
+import { AppRoute, PAGINATION_STEP } from '../../../../../../const';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { getCurrentPage } from '../../../../../../util';
 
-function Pagination(): JSX.Element {
+type PaginationProps = {
+  totalPageNumber: number;
+}
+
+function Pagination({totalPageNumber}: PaginationProps): JSX.Element {
   const location = useLocation();
 
-  const totalGuitarNumber = useSelector(getTotal);
-  const totalPageNumber = Math.ceil(totalGuitarNumber / ITEMS_PER_PAGE);
-
-  const getCurrentPage = (): number => {
-    if (location.pathname.includes(AppRoute.Page)) {
-      const splitPath = location.pathname
-        .split('/')[location.pathname.split('/').length-1];
-
-      return Number(splitPath[splitPath.length-1]
-        .split('_')[splitPath[splitPath.length-1].split('_').length-1]);
-    } else {
-      return 1;
-    }
-  };
-
-  const currentPage = getCurrentPage();
+  const currentPage = getCurrentPage(location.pathname);
 
   const position = Math.ceil(currentPage / PAGINATION_STEP);
   const positionStart = position * PAGINATION_STEP - (PAGINATION_STEP - 1);
@@ -76,32 +64,29 @@ function Pagination(): JSX.Element {
     }
   };
 
-  if (totalPageNumber === 0) {
-    return <div className="pagination page-content__pagination"></div>;
-  } else {
-    return (
-      <div className="pagination page-content__pagination">
-        <ul className="pagination__list">
-          {isPrevButton ?
-            <li className="pagination__page pagination__page--prev" id="prev">
-              <Link
-                className="link pagination__page-link"
-                to={`${AppRoute.Catalog}${AppRoute.Page}${positionStart - 1}${location.search}`}
-              >Назад
-              </Link>
-            </li> : ''}
-          {getPagination()}
-          {isNextButton ?
-            <li className="pagination__page pagination__page--next" id="next">
-              <Link
-                className="link pagination__page-link"
-                to={`${AppRoute.Catalog}${AppRoute.Page}${positionEnd + 1}${location.search}`}
-              >Вперед
-              </Link>
-            </li> : ''}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="pagination page-content__pagination">
+      <ul className="pagination__list">
+        {isPrevButton ?
+          <li className="pagination__page pagination__page--prev" id="prev">
+            <Link
+              className="link pagination__page-link"
+              to={`${AppRoute.Catalog}${AppRoute.Page}${positionStart - 1}${location.search}`}
+            >Назад
+            </Link>
+          </li> : ''}
+        {getPagination()}
+        {isNextButton ?
+          <li className="pagination__page pagination__page--next" id="next">
+            <Link
+              className="link pagination__page-link"
+              to={`${AppRoute.Catalog}${AppRoute.Page}${positionEnd + 1}${location.search}`}
+            >Вперед
+            </Link>
+          </li> : ''}
+      </ul>
+    </div>
+  );
 }
+
 export default Pagination;
