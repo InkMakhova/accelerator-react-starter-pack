695ce16c78b96c2a7e75d7c84795e6ffb7692507
Inna Makhova <i.m.makhova@gmail.com>
2022-01-14 11:15:00 +0200

    Рефакторинг навигации

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,9 @@
-import {Route, Router as BrowserRouter, Switch} from 'react-router-dom';
+import { Route, Router as BrowserRouter, Switch } from 'react-router-dom';
 import browserHistory from '../../browser-history';
 import CatalogPage from '../catalog-page/catalog-page';
-import {AppRoute} from '../../const';
+import { AppRoute, NavigationSection } from '../../const';
 import NotFoundPage from '../not-found-page/not-found-page';
+import UnderConstructionPage from '../under-construction-page/under-construction-page';
 
 function App(): JSX.Element {
   return (
@@ -16,6 +17,12 @@ function App(): JSX.Element {
         </Route>
         <Route exact path={`${AppRoute.Catalog}${AppRoute.Page}:page`}>
           <CatalogPage />;
+        </Route>
+        <Route exact path={AppRoute.WhereToBuy}>
+          <UnderConstructionPage currentNavigationSection={NavigationSection.WhereToBuy} />;
+        </Route>
+        <Route exact path={AppRoute.AboutUs}>
+          <UnderConstructionPage currentNavigationSection={NavigationSection.AboutUs} />;
         </Route>
         <Route >
           <NotFoundPage />;
