f40108c700c57970b1091189d1cd03046fc2a01e
Inna Makhova <i.m.makhova@gmail.com>
2022-01-15 15:37:30 +0200

    Рефакторинг тестов для корзины

--- src/components/common/header/components/cart/cart.test.tsx
+++ src/components/common/header/components/cart/cart.test.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { Router, Route, Switch} from 'react-router-dom';
 import Cart from './cart';
+import userEvent from '@testing-library/user-event';
+import {createMemoryHistory} from 'history';
 
-it('should render Cart correctly', () => {
-  const {container} = render(
-    <Router>
-      <Cart />
-    </Router>);
-  expect(container).toMatchSnapshot();
+const history = createMemoryHistory();
+
+describe('Component: Cart', () => {
+  it('should render Cart correctly', () => {
+    render(
+      <Router history={history}>
+        <Cart />;
+      </Router>);
+
+    expect(screen.getByTestId('cart')).toBeInTheDocument();
+    expect(screen.getByText('Перейти в корзину')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+
+  it('should redirect to cart url when user clicked to link', () => {
+    history.push('/fake');
+    render(
+      <Router history={history}>
+        <Switch>
+          <Route path="/cart" exact>
+            <h1>This is cart page</h1>
+          </Route>
+          <Route>
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>);
+
+    expect(screen.queryByText(/This is cart page/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('link'));
+    expect(screen.getByText(/This is cart page/i)).toBeInTheDocument();
+  });
 });
--- src/components/common/header/components/cart/cart.tsx
+++ src/components/common/header/components/cart/cart.tsx
@@ -3,7 +3,12 @@ import { Link } from 'react-router-dom';
 
 function Cart(): JSX.Element {
   return (
-    <Link className="header__cart-link" to={AppRoute.Cart} aria-label="Корзина">
+    <Link
+      className="header__cart-link"
+      to={AppRoute.Cart}
+      aria-label="Корзина"
+      data-testid="cart"
+    >
       <svg className="header__cart-icon" width="14" height="14" aria-hidden="true">
         <use xlinkHref="#icon-basket"></use>
       </svg>
