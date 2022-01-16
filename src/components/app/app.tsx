3b7fc53b8177c36730efcd2df872581c7729b353
Inna Makhova <i.m.makhova@gmail.com>
2022-01-16 20:28:37 +0200

    Тесты для App, рефакторинг App, index

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,4 @@
-import { Route, Router as BrowserRouter, Switch } from 'react-router-dom';
-import browserHistory from '../../browser-history';
+import { Route, Switch } from 'react-router-dom';
 import CatalogPage from '../catalog-page/catalog-page';
 import { AppRoute, NavigationSection } from '../../const';
 import NotFoundPage from '../not-found-page/not-found-page';
@@ -7,28 +6,26 @@ import UnderConstructionPage from '../under-construction-page/under-construction
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter history={browserHistory}>
-      <Switch>
-        <Route exact path={AppRoute.Root}>
-          <CatalogPage />;
-        </Route>
-        <Route exact path={AppRoute.Catalog}>
-          <CatalogPage />;
-        </Route>
-        <Route exact path={`${AppRoute.Catalog}${AppRoute.Page}:page`}>
-          <CatalogPage />;
-        </Route>
-        <Route exact path={AppRoute.WhereToBuy}>
-          <UnderConstructionPage currentNavigationSection={NavigationSection.WhereToBuy} />;
-        </Route>
-        <Route exact path={AppRoute.AboutUs}>
-          <UnderConstructionPage currentNavigationSection={NavigationSection.AboutUs} />;
-        </Route>
-        <Route >
-          <NotFoundPage />;
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path={AppRoute.Root}>
+        <CatalogPage />;
+      </Route>
+      <Route exact path={AppRoute.Catalog}>
+        <CatalogPage />;
+      </Route>
+      <Route exact path={`${AppRoute.Catalog}${AppRoute.Page}:page`}>
+        <CatalogPage />;
+      </Route>
+      <Route exact path={AppRoute.WhereToBuy}>
+        <UnderConstructionPage currentNavigationSection={NavigationSection.WhereToBuy} />;
+      </Route>
+      <Route exact path={AppRoute.AboutUs}>
+        <UnderConstructionPage currentNavigationSection={NavigationSection.AboutUs} />;
+      </Route>
+      <Route >
+        <NotFoundPage />;
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import browserHistory from './browser-history';
+import { Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router history={browserHistory}>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
