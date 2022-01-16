fc623cbdfaf5c48ba52511f2fed8aed93a69c4bd
Inna Makhova <i.m.makhova@gmail.com>
2022-01-16 19:25:09 +0200

    Тесты для карточки гитары

---
+++ src/mock/mock-guitar.ts
@@ -0,0 +1,11 @@
+export const mockGuitar = {
+  description: 'Практичная, удобная и простая гитара, созданная для музыкантов, только начинающих сфой путь в мир музыки. Она обладает всеми характеристиками, которые сделали работы компании Честер популярными.',
+  id: 8,
+  name: 'Честер Plus',
+  previewImg: 'img/guitar-7.jpg',
+  price: 30000,
+  rating: 4.5,
+  stringCount: 4,
+  type: 'electric',
+  vendorCode: 'SO135646',
+};
